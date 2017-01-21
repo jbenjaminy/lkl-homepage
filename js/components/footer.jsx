b48918d6a58bd1a3dd1804d19c10ecedf6e2a80f
jbenjaminy <young.benjamin@gmail.com>
2017-01-20 23:52:23 -0600

    adds responsive changes

--- js/components/footer.jsx
+++ js/components/footer.jsx
@@ -24,11 +24,11 @@ class Footer extends React.Component {
                                             <td className="inner-col" width="100%">
                                                 <div className='footer-left' width="100%">
                                                     <ul width="100%">
-                                                        <li><a onClick={this.changePage.bind(this, '/')}>HOME</a></li>
+                                                        <span className='a'><li><a onClick={this.changePage.bind(this, '/')}>HOME</a></li>
                                                         <li><a onClick={this.changePage.bind(this, '/about-us')}>ABOUT</a></li>
                                                         <li><a onClick={this.changePage.bind(this, '/current-projects')}>CURRENT PROJECTS</a></li>
                                                         <li><a onClick={this.changePage.bind(this, '/prior-projects')}>PRIOR PROJECTS</a></li>
-                                                        <li><a onClick={this.changePage.bind(this, '/contact-us')}>CONTACT</a></li>
+                                                        <li><a onClick={this.changePage.bind(this, '/contact-us')}>CONTACT</a></li></span>
                                                     </ul>
                                                 </div>
                                             </td>
@@ -41,10 +41,10 @@ class Footer extends React.Component {
                                             <td className="inner-col" width="100%">
                                                 <div className='footer-right' width="100%">
                                                     <ul width="100%">
-                                                        <li>LINGARD FITCH</li>
-                                                        <li><a href='tel:1-713-540-1577'>713-540-1577</a></li>
-                                                        <li><a href='mailto:info@lkldevelopmentgroup.com'>INFO@LKLDEVELOPMENTGROUP.COM</a></li>
-                                                        <li><a href='https://goo.gl/maps/H5Y7KN5JKMQ2'>4545 BISSONNET STREET, SUITE 285<br/>HOUSTON, TEXAS 77401</a></li>
+                                                        <li className='footer-name'>LKL DEVELOPMENT GROUP</li>
+                                                        <span className='a'><li><a href='tel:1-713-540-1577'>713-540-1577</a></li>
+                                                        <li><a href='mailto:info@lkldevelopmentgroup.com'>INFO@LKLDEVELOPMENTGROUP.COM</a></li></span>
+                                                        <li className='address'><a href='https://goo.gl/maps/H5Y7KN5JKMQ2'>4545 BISSONNET STREET, SUITE 285<br/>HOUSTON, TEXAS 77401</a></li>
                                                     </ul>
                                                 </div>
                                             </td>
@@ -58,8 +58,10 @@ class Footer extends React.Component {
                                                 <div className='footer-right' width="100%">
                                                     <ul width="100%">
                                                         <li>EXCLUSIVELY MARKETED BY:</li>
-                                                        <li>FOR INQUIRIES CONTACT ERIKA KHOURY</li>
-                                                        <li><a href='tel:1-713-707-4993'>713-707-4993</a></li>
+                                                        <li><a href='http://camelothouston.com'><img src='http://i.imgur.com/eI7Eh85.png' className='camelot-img'/></a></li>
+                                                        <li>FOR INQUIRIES CONTACT:</li>
+                                                        <span className='a'><li className='ek-name'><a href='mailto:erika@camelothouston.com'>ERIKA KHOURY</a></li>
+                                                        <li className='ek-phone'><a href='tel:1-713-707-4993'>713-707-4993</a></li></span>
                                                     </ul>
                                                 </div>
                                             </td>
