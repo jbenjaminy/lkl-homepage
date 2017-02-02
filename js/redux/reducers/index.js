47db9b0a0dfc33da7425655d04c501bfe8a95bcb
jbenjaminy <young.benjamin@gmail.com>
2017-02-02 00:53:20 -0600

    creates redux directory and adds in actions director and index file

--- js/redux/reducers/index.js
+++ js/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import ToggleReducer from './toggle-reducer';
 import PageReducer from './page-reducer';
 import ProjReducer from './proj-reducer';
 import SelectedReducer from './selected-reducer';
 
 export default combineReducers({
+    toggled: ToggleReducer,
     page: PageReducer,
     projects: ProjReducer,
     project: SelectedReducer,
