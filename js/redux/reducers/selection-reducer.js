62e78f9ebb064c3390b2a4a9ee5820395667361e
jbenjaminy <young.benjamin@gmail.com>
2017-02-26 23:53:51 -0600

    updates selection reducer

--- js/redux/reducers/selection-reducer.js
+++ js/redux/reducers/selection-reducer.js
@@ -14,54 +14,18 @@ const INITIAL_STATE = {
 };
 
 export default (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case 'SELECT_PAGE':
-            return action.data;
-        default:
-            return state;
-    }
-
-
-     const INITIAL_STATE = {
-         page: 1,
-         compartment: '',
-    // PAGE 1
-         name: '',
-         dosage: '',
-         // mg
-        //  appearance: null,
-        image: '',
-    // PAGE 2
-         howOften: '',
-        //  days interval (as needed, every day - every 29 days)
-         dailyDoses: '',
-    // PAGE 3
-        timePickerSelection: new Date(),
-        timeZoneOffsetInHours: (-1) * ((new Date()).getTimezoneOffset() / 60),
-        doseTimes: [],
-    // PAGE 4
-        rxNum: '',
-        pillsRemaining: '',
-        endDate: new Date()
-     };
-
-     export default (state = INITIAL_STATE, action) => {
-        switch (action.type) {
-            case MED_UPDATE:
-            /*
-             *  Because we are in the reducer, we always want to make sure we return
-                an object.
-             *  Square braces are not an array -- called 'key interpolation'.
-             *  Key that we're adding to the object here will be determined at
-                runtime.
-             *  If we call our action creator with the prop of 'name', the key set
-                for the value that we're adding to the object set to 'name'.
-             *  '...state' refers to default value for state.
-             */
-                return { ...state, [action.data.prop]: action.data.value };
-            case CLEAR_MED_FORM:
-                return INITIAL_STATE;
-            default:
-                return state;
-        }
-     };
+   switch (action.type) {
+       case SELECT_PAGE:
+           return { ...state, projects: action.data };
+       case SELECT_PROJECT:
+           return { ...state, project: action.data, showDetails: true };
+       case TOGGLE_NAV:
+           return { ...state, toggleNav: true };
+       case RESET_NAV:
+            return { ...state, toggleNav: false };
+       case RESET_STATE:
+           return INITIAL_STATE;
+       default:
+           return state;
+   }
+};
